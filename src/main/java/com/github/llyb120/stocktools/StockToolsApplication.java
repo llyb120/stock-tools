e0571c4bd179b6a3fc5b5e86da0910c8d9c3d7cd
llyb120 <llyb120@gmail.com>
2021-04-17 11:31:11 +0800

    first

---
+++ src/main/java/com/github/llyb120/stocktools/StockToolsApplication.java
@@ -0,0 +1,13 @@
+package com.github.llyb120.stocktools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockToolsApplication.class, args);
+    }
+
+}
